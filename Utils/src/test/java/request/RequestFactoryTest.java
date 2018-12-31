45e3610b74d7a46f1706f1482d05917b3748f6a8
Marcin <kruk.marcin@tlen.pl>
2018-12-31 14:50:15 +0100

    RequestFactory tests invalid cases added

--- Utils/src/test/java/request/RequestFactoryTest.java
+++ Utils/src/test/java/request/RequestFactoryTest.java
@@ -58,4 +58,46 @@ public class RequestFactoryTest {
         assertEquals(request.destinationHostId, 20);
         assertEquals(request.fileName, "file");
     }
+
+    @Test
+    public void RequestFactory_Request_Invalid() {
+        String requestStr = "tradalksd";
+        Request request = RequestFactory.getRequest(requesterId, requestStr);
+
+        assertEquals(request.requesterId, requesterId);
+        assertEquals(request.requestCode, RequestCode.UNKNOWN);
+    }
+
+    @Test
+    public void RequestFactory_Update_Invalid() {
+        String requestStr = "Update tralala 10 1l2kjjf3";
+        UpdateRequest request = (UpdateRequest) RequestFactory.getRequest(requesterId, requestStr);
+
+        assertEquals(request.requesterId, requesterId);
+        assertEquals(request.requestCode, RequestCode.UNKNOWN);
+        assertEquals(request.downloaded, 0);
+        assertEquals(request.uploaded, 0);
+        assertNull(request.fileName);
+    }
+
+    @Test
+    public void RequestFactory_Pull_Invalid() {
+        String requestStr = "pull";
+        PullRequest request = (PullRequest) RequestFactory.getRequest(requesterId, requestStr);
+
+        assertEquals(request.requesterId, requesterId);
+        assertEquals(request.requestCode, RequestCode.UNKNOWN);
+        assertNull(request.fileName);
+    }
+
+    @Test
+    public void RequestFactory_Push_Invalid() {
+        String requestStr = "push file 123123l";
+        PushRequest request = (PushRequest) RequestFactory.getRequest(requesterId, requestStr);
+
+        assertEquals(request.requesterId, requesterId);
+        assertEquals(request.requestCode, RequestCode.UNKNOWN);
+        assertEquals(request.destinationHostId, 0);
+        assertNull(request.fileName);
+    }
 }

421c6a28e29abde70a8954431df42541fc3d2a48
Marcin <kruk.marcin@tlen.pl>
2018-12-31 13:32:10 +0100

    TrackedTorrent tests fixup

--- Tracker/src/test/java/TrackedTorrentTest.java
+++ Tracker/src/test/java/TrackedTorrentTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.InetSocketAddress;
@@ -6,49 +7,63 @@ import static org.junit.Assert.*;
 
 public class TrackedTorrentTest {
 
-    private final ClientMetadata clientA = new ClientMetadata(1, new InetSocketAddress("localhost", 1));
-    private final ClientMetadata clientB = new ClientMetadata(2, new InetSocketAddress("localhost", 2));
     private final FileMetadata fileMetadata = new FileMetadata("A", 10, "MdA");
+    private TrackedPeer peerA = new TrackedPeer(new ClientMetadata(1, new InetSocketAddress(1)));
+    private TrackedPeer peerB = new TrackedPeer(new ClientMetadata(2, new InetSocketAddress(2)));
+
+    private TrackedTorrent torrent;
+
+    @Before
+    public void setup() {
+        torrent = new TrackedTorrent(fileMetadata, peerA);
+    }
 
     @Test
     public void TrackedTorrent_Creation() {
-        TrackedPeer peer = new TrackedPeer(clientA);
-        TrackedTorrent torrent = new TrackedTorrent(fileMetadata, peer);
+        TrackedTorrent torrent = new TrackedTorrent(fileMetadata, peerA);
 
         assertTrue(torrent.hasAnyPeer());
         assertEquals(torrent.fileMetadata, fileMetadata);
-        assertEquals(torrent.getPeerById(peer.id).get(), peer);
+        assertTrue(torrent.getPeerById(peerA.id).isPresent());
     }
 
     @Test
-    public void TrackedTorrent_AddPeer() {
-        TrackedPeer peerA = new TrackedPeer(clientA);
-        TrackedPeer peerB = new TrackedPeer(clientB);
-
-        TrackedTorrent torrent = new TrackedTorrent(fileMetadata, peerA);
-        torrent.addPeer(peerB);
-
-        assertTrue(torrent.hasAnyPeer());
-        assertEquals(torrent.fileMetadata, fileMetadata);
-        assertTrue(torrent.getPeerById(1).isPresent());
-        assertEquals(torrent.getPeerById(1).get(), peerA);
-        assertTrue(torrent.getPeerById(2).isPresent());
-        assertEquals(torrent.getPeerById(2).get(), peerB);
-    }
-
-    @Test
-    public void TrackedTorrent_RemovePeer() {
-        TrackedPeer peerA = new TrackedPeer(clientA);
-        TrackedPeer peerB = new TrackedPeer(clientB);
+    public void TrackedTorrent_RemovePeer_singlePeer() {
         TrackedTorrent torrent = new TrackedTorrent(fileMetadata, peerA);
 
-        torrent.removePeer(peerA);
-        assertFalse(torrent.hasAnyPeer());
+        torrent.removePeer(peerA.id);
         assertFalse(torrent.getPeerById(peerA.id).isPresent());
+        assertFalse(torrent.hasAnyPeer());
+    }
 
+    @Test
+    public void TrackedTorrent_AddPeer_multiplePeers() {
         torrent.addPeer(peerB);
+
         assertTrue(torrent.hasAnyPeer());
+        assertTrue(torrent.getPeerById(peerA.id).isPresent());
         assertTrue(torrent.getPeerById(peerB.id).isPresent());
+    }
+
+    @Test
+    public void TrackedTorrent_RemovePeer_multiplePeers() {
+        torrent.addPeer(peerB);
+        torrent.removePeer(peerA);
+
+        assertFalse(torrent.getPeerById(peerA.id).isPresent());
+        assertTrue(torrent.getPeerById(peerB.id).isPresent());
+        assertTrue(torrent.hasAnyPeer());
+    }
+
+    @Test
+    public void TrackedTorrent_RemovePeer_allPeers() {
+        torrent.addPeer(peerB);
+        torrent.removePeer(peerA);
+        torrent.removePeer(peerB);
+
+        assertFalse(torrent.getPeerById(peerA.id).isPresent());
+        assertFalse(torrent.getPeerById(peerB.id).isPresent());
+        assertFalse(torrent.hasAnyPeer());
     }
 
 }

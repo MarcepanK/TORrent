76353333555c05769a89c9826dfeb8b838e5c926
Marcin <kruk.marcin@o2.pl>
2019-01-19 22:00:57 +0100

    removed unused constructor

--- Common/src/main/java/common/ClientHandshake.java
+++ Common/src/main/java/common/ClientHandshake.java
@@ -7,15 +7,7 @@ public class ClientHandshake implements Serializable {
 
     public final int id;
     public final FileMetadata[] ownedFilesMetadata;
-
-    public ClientHandshake(int id, File[] files) {
-        this.id = id;
-        ownedFilesMetadata = new FileMetadata[files.length];
-        for(int i=0; i<files.length; i++) {
-            ownedFilesMetadata[i] = new FileMetadata(files[i]);
-        }
-    }
-
+    
     public ClientHandshake(int id, FileMetadata[] ownedFilesMetadata) {
         this.id = id;
         this.ownedFilesMetadata = ownedFilesMetadata;
